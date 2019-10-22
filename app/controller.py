66de40a06df180ea175d914ed234cfd14d2416d8
Santos <gabrielsantos.s.guedes@gmail.com>
2019-10-21 22:02:55 -0300

    loop de boas vindas

--- app/controller.py
+++ app/controller.py
@@ -1,9 +1,25 @@
+import time
+
 from app.web_driver import driver
 
 class Account:
     def __init__(self):
+        self.dvr = None
         self.msg = ''
 
     def autenticar(self):
-        image = driver().search_img()
+        self.dvr = driver()
+        image = self.dvr.search_img()
         return image
+
+    def identify_new_message(self):
+
+        while True:
+            new = self.dvr.check_notification()
+
+            if (new):
+                print('Nova notificação')
+                self.dvr.give_welcome()
+            else:
+                print('Nenhuma nova conversa identificada!')
+                time.sleep(10)
--- app/web_driver.py
+++ app/web_driver.py
@@ -3,15 +3,17 @@ import time
 from selenium import webdriver
 import os
 
+from app.mensagens import Mensagem
+
+
 class driver:
     def __init__(self):
-
         self.webdriver = webdriver.Chrome(chrome_options=self.configure())
 
     def configure(self):
         options = webdriver.ChromeOptions()
         options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36')
-        options.add_argument('--headless')
+        # options.add_argument('--headless')
         return options
 
     def search_img(self):
@@ -21,14 +23,19 @@ class driver:
 
     def check_notification(self):
         new = self.webdriver.find_elements_by_class_name('P6z4j')
-
-        if (len(new) > 1):
-            return True
-        else:
-            return False
+        return len(new)
         #  _1ZMSM de fora
         #  P6z4j de dentro
 
     def give_welcome(self):
         # continuar
+        chat_list = self.webdriver.find_elements_by_class_name('X7YrQ')
+        for chat in chat_list:
+            content = chat.find_elements_by_class_name('P6z4j')
+            if (content):
+                chat.click()
+                time.sleep(1)
+                self.webdriver.find_element_by_class_name('_3u328').send_keys(
+                    Mensagem.welcome('Clinica  médica Getulio Vargas'))
+
         pass
